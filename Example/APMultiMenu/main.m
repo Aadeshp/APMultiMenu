0da7191d79d3aaadad2488d73da4f5679cb0ba13
aadesh <aadeshp95@gmail.com>
2015-02-07 20:29:07 -0500

    Initial commit

---
+++ Example/APMultiMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  APMultiMenu
+//
+//  Created by aadesh on 02/07/2015.
+//  Copyright (c) 2014 aadesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APMultiMenuAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APMultiMenuAppDelegate class]));
+    }
+}
